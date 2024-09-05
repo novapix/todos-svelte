76523bf2e25ba5ae54bb51952d4ba3400fd601d6
Parakram <parakram@keemail.me>
2024-09-06 00:16:18 +0545

    Persist Daisyui theme change
    Use svelte onMount to set toggles

--- src/lib/components/ThemeChanger.svelte
+++ src/lib/components/ThemeChanger.svelte
@@ -1,20 +1,41 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	let darkMode = false;
+
+	function toggleTheme() {
+		darkMode = !darkMode;
+		applyTheme();
+	}
+
+	function applyTheme() {
+		console.log('Applying theme, darkMode:', darkMode);
+		if (browser) {
+			localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+			document.documentElement.setAttribute('data-theme', darkMode ? 'dark' : 'light');
+		}
+	}
+
+	onMount(() => {
+		if (browser) {
+			const theme = localStorage.getItem('theme');
+			const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+			darkMode = theme === 'dark' || (!theme && prefersDarkScheme);
+			// applyTheme();
+		}
+	});
+
+	function handleToggleChange(event: Event) {
+		const target = event.target as HTMLInputElement;
+		console.log('Toggle changed, checked:', target.checked);
+		darkMode = target.checked;
+		applyTheme();
+	}
+</script>
+
 <div class="flex-none">
 	<label class="flex cursor-pointer gap-2">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="20"
-			height="20"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		>
-			<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-		</svg>
-		<input type="checkbox" value="light" class="toggle theme-controller" />
-
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="20"
@@ -30,6 +51,20 @@
 			<path
 				d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
 			/>
+		</svg>
+		<input type="checkbox" class="toggle" checked={darkMode} on:change={handleToggleChange} />
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="20"
+			height="20"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		>
+			<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
 		</svg>
 	</label>
 </div>
