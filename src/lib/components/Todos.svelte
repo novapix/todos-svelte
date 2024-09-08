56c366b39e14756f9ad418fca985492ee5e30cdf
Parakram <parakram@keemail.me>
2024-09-08 11:20:31 +0545

    add text box for adding todo

--- src/lib/components/Todos.svelte
+++ src/lib/components/Todos.svelte
@@ -1,4 +1,25 @@
 <main>
-    <div class="">
-    </div>
+	<section class="todos container">
+		<div
+			class="flex items-center justify-center mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8"
+		>
+			<form class="flex items-center space-x-4">
+				<div class="flex items-center justify-center">
+					<input
+						type="checkbox"
+						class="form-checkbox h-6 w-6 text-blue-600 bg-white border-gray-300 rounded"
+					/>
+					<label class="sr-only" aria-label="Mark all as complete" for="toggle-all">
+						Mark all as complete
+					</label>
+				</div>
+				<input
+					id="new-todo"
+					class="input input-bordered w-full max-w-lg h-12 px-4 text-lg"
+					placeholder="What needs to be done?"
+					type="text"
+				/>
+			</form>
+		</div>
+	</section>
 </main>
